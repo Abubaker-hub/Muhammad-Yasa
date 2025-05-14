404a791a44535c92028ce36631bbb0ce7b0970a5
Abubaker-hub <abubakersaddique691@gmail.com>
2025-05-14 06:59:51 -0700

    Create Yasa.c

---
+++ Yasa.c
@@ -0,0 +1,143 @@
+// int = 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 
+// float = -4, -6, -7, 7.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5, 1, 7.0
+// char = a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z
+// string = "Hello World"
+// boolean = true, false
+// null = null
+// undefined = undefined
+// object = object
+// array = array
+// function = function
+// class = class
+// interface = interface
+// enum = enum
+// typedef = typedef
+// #include = #include
+// #pragma = #pragma
+// #ifdef = #ifdef
+// #ifndef = #ifndef
+// #define = #define
+// #include = #include
+
+
+
+
+
+
+
+// #include <stdio.h>
+// /*
+// print table of the number entered2  by the user in pretty form
+
+
+// Example:
+// input 3
+// Enter multiple table are;
+
+// output 2
+// Enter multiple table are;
+// 2 * 1 = 2
+// 2 * 2= 4
+// 2 * 3 = 6
+// 2 * 4 = 8
+// 2 * 5 = 10
+// 2 * 6 = 12
+// 2 * 7 = 14
+// 2 * 8 = 16
+// 2 * 9 = 18
+// 2 * 10 + 20
+// */
+//     int main ()
+// {
+//     /*code*/
+//     return 0;
+// }
+// #include <stdio.h>
+// #define Pi 3.14
+// const int Year =1996;
+// int main()
+// {
+//     int a,b;
+//     float x,y;
+//     a=10,b=3;
+//     x=5.5;y=2.0;
+//     printf("Integer operations:/n");
+//     printf("a+b=%d/n",a+b);
+//     printf("a-b=%d/n",a-b);
+//     printf("a*b=%d/n",a+b);
+//     printf("a/b=%d/n",a+b);
+//     printf("a%%b=%d/n",a+b);
+//     //float operations
+//     printf("x+y=%2.f/n",x+y);
+//     printf("x*y=%2.f/n",x*y);
+//     printf("x-y=%2.f/n",x-y); 
+//     printf("x/y=%2.f/n",x/y);
+//     //using constants
+//     printf("/nUsing constants:/n");
+//     printf("Value of Pi=%.2f/n",Pi);
+//     printf("Current year =%d/n",Year);
+//     return 0;
+// }
+
+
+
+// #include <stdio.h>
+// int main ()
+// {
+//     int age;
+//     printf("Enter your age: ");
+//     scanf("%d",&age);
+//     printf("My age is %d\n", age);
+//     return 0;
+// }
+// #include <stdio.h>
+// int main ()
+// {
+//     int ali = 10; 
+//     float age  = 7.8;
+//     printf("Enter my age %d");
+//     scanf("%d",&age);
+//     printf("My age is %f/n ");
+//     scanf("%f",&age);
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
